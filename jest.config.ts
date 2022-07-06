ace5ca0c23f34fa7764f4ea386974d6cea19afe7
Matheus Fonseca Chaves <matheusfonsecachaves@gmail.com>
2022-07-06 01:22:12 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
